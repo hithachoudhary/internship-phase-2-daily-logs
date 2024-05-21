563351c7e1607edbf337d1080c7cba01870d4145
hithachoudhary <hithachoudhary5559@gmail.com>
2024-05-21 11:42:24 +0530

    Created the solution file for discussion-25

--- Day-17/mini-projects/discussion-25-solutions.sql
+++ Day-17/mini-projects/discussion-25-solutions.sql
@@ -7,7 +7,7 @@
     --Create a view to identify the most purchased product of each day.
     create view daily_top_product as select date(orderDate) as order_date,productCode,sum(quantityOrdered) as total_quantity from orders join orderdetails using (orderNumber) group by order_date,productCode order by total_quantity desc;
     --Finally, combine these views to produce the required daily report.
-    select ds.order_date,ds.total_sales,do.num_of_orders,dtp.productCode as top_product,dtp.total_quantity from daily_sales ds inner join daily_orders do on ds.order_date = do.order_date inner join (select dtp1.order_date,dtp1.productCode,dtp1.total_quantity from daily_top_product dtp1 inner join(select order_date, max(total_quantity) as maximum_quantity from daily_top_product group by order_date)dtp2 on dtp1.order_date = dtp2.order_date and dtp1.total_quantity = dtp2.maximum_quantity)dtp on ds.order_date = dtp.order_date;
+    select distinct order_date, total_sales, no_of_orders, (select productCode from daily_top_products where daily_top_products.order_date=daily_total_sales.order_date limit 1) as top_product from daily_total_sales join daily_orders using(order_date) join daily_top_products using(order_date);
 
 --Scenario 2: Sales Monitoring System
     --Create a view that shows the total number of customers handled by each sales rep.
@@ -20,7 +20,7 @@
     create view rep_performance as select rc.salesRepEmployeeNumber,rc.num_of_customers,rp.total_payments,ro.num_of_orders from rep_customers rc inner join rep_payments rp on rc.salesRepEmployeeNumber = rp.salesRepEmployeeNumber inner join rep_orders ro on rc.salesRepEmployeeNumber = ro.salesRepEmployeeNumber;
 
 --Scenario 3: HR and Sales Data Analysis
-    --Create a view in the hr database that shows the department and age of each employee.
+    --Create a view in the hr database that shows the department and age of each employee, ensuring that the employee table has the column birth_date.
     create view emp_details as select employee_id,department_id,timestampdiff(year, birth_date, curdate()) as age from employees;
     --Create a view in the classicmodels database that shows the sales performance of each employee.
-    create view emp_sales_performance as select employeeNumber,sum(amount) as total_sales from customers inner join payments using (customerNumber) group by employeeNumber;
+    create view emp_sales_performance as select salesRepEmployeeNumber,sum(amount) as total_sales from customers inner join payments using (customerNumber) group by salesRepEmployeeNumber;
